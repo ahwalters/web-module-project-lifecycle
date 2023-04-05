ff18e1daae4d6240692a28091de7aa1e566603ef
Alana Walters <alana.walters@yahoo.com>
2023-04-04 22:26:30 -0400

    working with Zac

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import axios from 'axios';
+import { debug } from 'webpack';
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos : [],
+      error : '',
+      todoNameInput : ''
+    }
+  }
+
+  onNameInputChange = (evt) => {
+    const value = evt.target.value
+    debugger
+    }
+
+
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+    .then(res => {
+      this.setState({...this.state, todos : res.data.data})
+    })
+    .catch(err => {
+      this.setState({...this.state, error : err.response.data.message})
+    })
+  }
+
+ 
+
+  componentDidMount() {
+    this.fetchAllTodos()
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <div id='error'>Error : {this.state.error}</div>
+        <div id='todos'>
+          <h2>Todos</h2>
+          {
+            this.state.todos.map(td => {
+              return <div key={td.id}>{td.name}</div>
+            })
+          }
+        </div>
+        <form id='todoForm'>
+          <input onChange={this.onNameInputChange} value={this.state.todoNameInput} type='text' placeholder='Type Your Todo' />
+          <button>Submit</button>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
   }
 }
